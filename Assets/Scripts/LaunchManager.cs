7b053ee41312c77f26ee82588a9755a9b2936b28
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-27 21:52:11 +0100

    First connection with server established, Canvas created for the Entry scene

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // This method is called on connection to the master server (Photon Server)
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to the photon server.");
+    }
+
+    // This method is called on connection to the Internet
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to the Internet");
+    }
+}
