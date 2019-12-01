c8dd1874d56068e07aef602485bcb4beccdd1075
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-01 21:49:48 +0100

    Double window control on a same device fixed

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    [SerializeField] GameObject FPSCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ControlOnlyMyPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Disable the movement controller and camera on other players
+    // This is needed to test game on the same computer with multiple game windows 
+    private void ControlOnlyMyPlayer()
+    {
+        if (photonView.IsMine)
+        {
+            transform.GetComponent<MovementController>().enabled = true;
+            FPSCamera.GetComponent<Camera>().enabled = true;
+        }
+        else
+        {
+            transform.GetComponent<MovementController>().enabled = false;
+            transform.GetComponent<Camera>().enabled = false;
+        }
+    }
+}
