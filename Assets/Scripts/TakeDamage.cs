500f645cff1e39dff48d928d1dc32abebcff18d1
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-01 22:59:34 +0100

    Weapon Aim added, Health bar decreasing in multiplayer added

--- Assets/Scripts/TakeDamage.cs
+++ Assets/Scripts/TakeDamage.cs
@@ -2,27 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class TakeDamage : MonoBehaviour
 {
-    public float health;
+    [SerializeField] Image healthBar;
+    private float health;
+    public float initialHealth = 100;
+
 
     // Start is called before the first frame update
     void Start()
     {
         health = 100;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        // We need to get a ratio of health for Image filling
+        healthBar.fillAmount = health / initialHealth;
     }
 
     // Remote procedure call to apply this method for all clients
-    [PunRPC] public void ReceiveDamage(float damage)
+    [PunRPC]
+    public void ReceiveDamage(float damage)
     {
         health -= damage;
         Debug.Log(health);
+
+        healthBar.fillAmount = health / initialHealth;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+
     }
 }
